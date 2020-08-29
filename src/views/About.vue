ee99a9e44db24db3a1c7b584a236434d1f12c9dd
yiying <yiying@daojia-inc.com>
2020-08-30 00:46:09 +0800

    feat: about更新

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,33 @@
       <div class="body">
           <div class="title">为什么要做这个网站？</div>
 
-          <body class="htmledit_views cke_editable cke_editable_themed cke_contents_ltr cke_show_borders" contenteditable="true" spellcheck="false"><h3>主要想解决如下几个痛点</h3><ul><li><p>离职，无法再看到内网的每周好文章分享</p></li><li><p>解决文章的关键字查询问题</p></li><li><p>解决个人分享归档问题</p></li></ul><h3>预期</h3><p>在信息爆炸的社会，我们可以很方便的获取信息，但是获取高质量的信息很困难。需要一个个的人肉验证信息的质量是否够高，希望这里成为高质量信息的集散地，相互节约各自人肉高质量信息的时间。</p><h3>计划</h3><ul><li><p>增加评论系统</p></li><li><p>增加积分系统</p></li><li><p>增加关注系统</p></li><li><p>增加收藏系统</p></li><li><p>增加个人推荐与收藏导出markdown</p></li><li><p>增加类Stackoverflow的权限系统</p></li><li><p>有想要的功能可以在github上提issue，或者自己撸起衣袖干！</p></li></ul><blockquote><p>GitHub: https://github.com/digdigwa/digdigdig.github.io.git</p></blockquote></body>
+          <div class="layout__panel layout__panel--preview" style=" font-size: 15px;"><div class="gutter" style="left: 0px;"><!----></div> <div class="preview"><div class="preview__inner-1"><div class="preview__inner-2 markdown_views prism-atom-one-dark" style="padding: 10px 16px 293px;"><div class="cl-preview-section"><h3><a id="_2"></a>主要想解决如下几个痛点</h3>
+</div><div class="cl-preview-section"><ul>
+<li>离职，无法再看到内网的团队每周好文章分享</li>
+<li>解决文章的关键字查询问题</li>
+<li>解决个人分享归档问题</li>
+</ul>
+</div><div class="cl-preview-section"><h3><a id="_7"></a>预期</h3>
+</div><div class="cl-preview-section"><p>现如今，我们可以很方便的在互联网上获取到各种各样的信息，但是，获取高质量的信息很困难。需要一个个的人肉验证信息的质量是否够高，希望这里成为高质量信息的集散地，相互节约各自人肉高质量信息的时间。</p>
+</div><div class="cl-preview-section"><h3><a id="_10"></a>后续想做的东西</h3>
+</div><div class="cl-preview-section"><ul>
+<li>增加标签系统</li>
+<li>增加评论系统</li>
+<li>增加积分系统</li>
+<li>增加关注系统</li>
+<li>增加收藏系统</li>
+<li>移动端小程序</li>
+<li>增加chrome快速推荐插件</li>
+<li>增加个人推荐与收藏导出markdown</li>
+<li>增加类Stackoverflow的权限系统</li>
+<li>有想要的功能可以在github上提issue，或者自己撸起衣袖干！</li>
+</ul>
+</div><div class="cl-preview-section"><blockquote>
+<p>GitHub: https://github.com/digdigwa/digdigdig.github.io.git</p>
+</blockquote>
+</div><div class="cl-preview-section"><h3><a id="_25"></a>关于域名</h3>
+</div><div class="cl-preview-section"><p>一次性买了10年，希望十年后还在！~ 挖挖挖… 哇哇哇…</p>
+</div></div></div> <!----></div></div>
 
       </div>
   </PageFrame>
