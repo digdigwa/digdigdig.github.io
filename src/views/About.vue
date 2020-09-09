97872417eef5f462ba3b01c342eea2ca9b660acd
yiying <yiying@daojia-inc.com>
2020-09-09 10:49:36 +0800

    feat: about更新

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,17 +2,17 @@
   <div class="body">
       <div class="title">为什么要做这个网站？</div>
 
-      <div class="layout__panel layout__panel--preview" style=" font-size: 15px;"><div class="gutter" style="left: 0px;"><!----></div> <div class="preview"><div class="preview__inner-1"><div class="preview__inner-2 markdown_views prism-atom-one-dark" style="padding: 10px 16px 293px;"><div class="cl-preview-section"><h3><a id="_2"></a>主要想解决如下几个痛点</h3>
+      <div class="layout__panel layout__panel--preview" style="width: 514px; font-size: 15px;"><div class="gutter" style="left: 0px;"><!----></div> <div class="preview"><div class="preview__inner-1"><div class="preview__inner-2 markdown_views prism-atom-one-dark" style="padding: 10px 16px 110px;"><div class="cl-preview-section"><h3><a id="_1"></a>主要想解决如下几个痛点</h3>
       </div><div class="cl-preview-section"><ul>
-      <li>离职，无法再看到内网的团队每周好文章分享</li>
-      <li>解决文章的关键字查询问题</li>
+      <li>打破内网壁垒，提供一个公共的场所分享有意思、有价值的文章</li>
+      <li>解决常规Markdown技术周报的关键字查询困难问题</li>
       <li>解决个人分享归档问题</li>
+      <li>解决推荐文章分类问题</li>
       </ul>
       </div><div class="cl-preview-section"><h3><a id="_7"></a>预期</h3>
       </div><div class="cl-preview-section"><p>现如今，我们可以很方便的在互联网上获取到各种各样的信息，但是，获取高质量的信息很困难。需要一个个的人肉验证信息的质量是否够高，希望这里成为高质量信息的集散地，相互节约各自人肉高质量信息的时间。</p>
       </div><div class="cl-preview-section"><h3><a id="_10"></a>后续想做的东西</h3>
       </div><div class="cl-preview-section"><ul>
-      <li>增加标签系统</li>
       <li>增加评论系统</li>
       <li>增加积分系统</li>
       <li>增加关注系统</li>
@@ -26,8 +26,10 @@
       </div><div class="cl-preview-section"><blockquote>
       <p>GitHub: https://github.com/digdigwa/digdigdig.github.io.git</p>
       </blockquote>
-      </div><div class="cl-preview-section"><h3><a id="_25"></a>关于域名</h3>
+      </div><div class="cl-preview-section"><h3><a id="_24"></a>关于域名</h3>
       </div><div class="cl-preview-section"><p>一次性买了10年，希望十年后还在！~ 挖挖挖… 哇哇哇…</p>
+      </div><div class="cl-preview-section"><h3><a id="_28"></a>其它</h3>
+      </div><div class="cl-preview-section"><p>可在chrome应用商店搜索DDD，下载一键推荐插件。或者点击<a href="http://digdigdig.vip/static/ddd-plugin.zip">这里</a>下载，在chrome的扩展程序中，点击【加载已解压的扩展程序】导入插件使用</p>
       </div></div></div> <!----></div></div>
 
   </div>
